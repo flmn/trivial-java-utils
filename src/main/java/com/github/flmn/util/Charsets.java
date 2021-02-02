0e5b999487c09b4d022fa172e8440857891d7d44
jitao <jitao@jitao.tech>
2021-02-02 13:16:15 +0800

    Charsets

--- src/main/java/com/github/flmn/util/Charsets.java
+++ src/main/java/com/github/flmn/util/Charsets.java
@@ -3,18 +3,14 @@ package com.github.flmn.util;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
-public final class Charsets {
-
-    public static final Charset US_ASCII = StandardCharsets.US_ASCII;
-    public static final Charset ISO_8859_1 = StandardCharsets.ISO_8859_1;
-    public static final Charset UTF_8 = StandardCharsets.UTF_8;
-    public static final Charset UTF_16BE = StandardCharsets.UTF_16BE;
-    public static final Charset UTF_16LE = StandardCharsets.UTF_16LE;
-    public static final Charset UTF_16 = StandardCharsets.UTF_16;
-    public static final Charset GBK = Charset.forName("GBK");
-    public static final Charset GB2312 = Charset.forName("GB2312");
-    public static final Charset BIG5 = Charset.forName("BIG5");
-
-    private Charsets() {
-    }
+public interface Charsets {
+    Charset US_ASCII = StandardCharsets.US_ASCII;
+    Charset ISO_8859_1 = StandardCharsets.ISO_8859_1;
+    Charset UTF_8 = StandardCharsets.UTF_8;
+    Charset UTF_16BE = StandardCharsets.UTF_16BE;
+    Charset UTF_16LE = StandardCharsets.UTF_16LE;
+    Charset UTF_16 = StandardCharsets.UTF_16;
+    Charset GBK = Charset.forName("GBK");
+    Charset GB2312 = Charset.forName("GB2312");
+    Charset BIG5 = Charset.forName("BIG5");
 }
